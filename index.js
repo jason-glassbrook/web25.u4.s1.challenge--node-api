00e916f4318dee3049e992aed54a7640152a4872
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-17 11:39:47 -0500

    setup index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+/**********************************************************/
+
+require ('dotenv').config ()
+const { clog } = require ('./tools')
+
+const server = require ('./server.js')
+const port = process.env.PORT || 5555
+
+server.listen (port, () => {
+  clog (`it's alive!`)
+  clog (`\n>>> listening on port ${port} <<<\n`)
+})
+
+/**************************************/
+
 /*
 play this: https://www.youtube.com/watch?v=d-diB65scQU
 
