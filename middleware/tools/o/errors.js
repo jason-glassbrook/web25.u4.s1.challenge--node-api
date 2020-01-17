60d768ad3a4f1ff4735de9c19cde0e81c1ba52b9
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-17 17:49:04 -0500

    simplify error messaging

--- middleware/tools/o/errors.js
+++ middleware/tools/o/errors.js
@@ -8,33 +8,27 @@ const { error } = require ('../')
   MAIN
 ***************************************/
 
-const buildRestOfError = (request, restOfError) => ({
-  method : request.method,
-  route : request.originalUrl,
-  ...restOfError,
-})
+const errorResponse = (defaultErrorMessage) => (
+  (restOfErrorMessage = '', restOfError = {}) => (
+    (ri, ro) => {
+      const responseErrorMessage = defaultErrorMessage + restOfErrorMessage
+
+      const restOfResponseError = {
+        method : ri.method,
+        route : ri.originalUrl,
+        ...restOfError,
+      }
+
+      return error (responseErrorMessage, restOfResponseError)
+    }
+  )
+)
 
 const errors = {
-  '400' :
-    (restOfErrorMessage = '', restOfError = {}) => (request) => (error (
-      `bad request` + restOfErrorMessage,
-      buildRestOfError (request, restOfError),
-    )),
-  '404' :
-    (restOfErrorMessage = '', restOfError = {}) => (request) => (error (
-      `resource not found` + restOfErrorMessage,
-      buildRestOfError (request, restOfError),
-    )),
-  '500' :
-    (restOfErrorMessage = '', restOfError = {}) => (request) => (error (
-      `something bad happened` + restOfErrorMessage,
-      buildRestOfError (request, restOfError),
-    )),
-  '501' :
-    (restOfErrorMessage = '', restOfError = {}) => (request) => (error (
-      `not implemented ... yet?` + restOfErrorMessage,
-      buildRestOfError (request, restOfError),
-    )),
+  '400' : errorResponse ('bad request'),
+  '404' : errorResponse ('resource not found'),
+  '500' : errorResponse ('something bad happened'),
+  '501' : errorResponse ('not implemented ... yet?'),
 }
 
 /**************************************/
