ad0a858fb4292a5a576a372410e5b54f166428bd
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-17 12:11:00 -0500

    /api/projects: setup router

---
+++ api/projects/router.js
@@ -0,0 +1,21 @@
+/***********************************************************
+  ~/projects - router
+***********************************************************/
+
+/// tools ///
+const express = require ('express')
+const { respondWithError } = require ('../../middleware')
+
+/***************************************
+  setup router
+***************************************/
+
+const router = express.Router ()
+
+/// requests ///
+router.route ('*')
+  .all (respondWithError (501))
+
+/**************************************/
+
+module.exports = router
